0dce7ae35d2d1a3696ed657c200f6a9f7a908c2a
Leticia <let@klinify.com>
2021-07-17 23:11:12 +0800

    Fixes for ICO migration script

--- ico/migrations/2_deploy_contracts.js
+++ ico/migrations/2_deploy_contracts.js
@@ -8,5 +8,9 @@ module.exports = function(deployer) {
   deployer.deploy(ICOToken, supplyConfig['token_supply']).then(function() {
     // Token price is 0.001 Ether
     return deployer.deploy(ICOTokenSale, ICOToken.address, supplyConfig['token_price']);
+  }).then(function() {
+    ICOToken.deployed().then((instance) => {
+      instance.transfer(ICOTokenSale.address, supplyConfig['token_circulating_supply'], { from: supplyConfig['admin_account_address'] });
+    });
   });
 };
